4639d217c25c1a1cd6a9efae35450f004571d51a
Alex <afraser281@gmail.com>
2022-02-15 12:17:10 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,16 @@ import { getRandomNews } from '../lib/function';
 
 
 export async function getStaticProps() {
-  const res = await fetch(process.env.APIpath + "/api/newsApi");
-  const json = await res.json();
+  try{
+    const data = await fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=tzDIKDLrSJUtyJQIXep7Y76qMee1Ucpz');
+    const json = await data.json();
+    
+
+      res.status(200).json({success:true,data:json});
+  }catch{
+      res.status(500).json({success:false,error:error.message});
+
+  }
   
   const data = getRandomNews(json);
   return{
